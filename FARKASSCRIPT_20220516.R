0dec09bc857c6d424a95eb5c2d3308f54fa63783
aranyics <aranyics11@gmail.com>
2023-01-25 20:31:08 +0100

    updated datainput with filtering high/low human density sites

--- FARKASSCRIPT_20220516.R
+++ FARKASSCRIPT_20220516.R
@@ -18,7 +18,8 @@ create.figdirs( )
 # Input data
 extended.data = FALSE # az elemzesek nincsenek beallitva a plusz fajokkal kibovitett tablazatra
 covid.data = 'pre' # pre, vagy post, amit a 2020-01-17 datum valaszt el. barmi mas eseten a teljes adathalmazt olvassa be
-f.df = read.bnp.farkas.data( extended = extended.data, covid = 'pre' )
+human.density = 'none'
+f.df = read.bnp.farkas.data( extended = extended.data, hum.den = human.density, covid = 'pre' )
 ##weather = read.weather.lunar.data()
 ##f.df <- merge(f.df, weather, by.x = 'Datum', by.y = 'Date')
 
@@ -30,13 +31,13 @@ f.df$tavasz <- sapply(f.df$Datum, function(y) as.numeric(str_split(as.character(
 f.df$nyar <- sapply(f.df$Datum, function(y) as.numeric(str_split(as.character(y), '-')[[1]][2]) %in% 3:8)
 f.df$vadaszideny <- sapply(f.df$Datum, function(y) as.numeric(str_split(as.character(y), '-')[[1]][2]) %in% c(1, 2, 9, 10, 11, 12))
 
-# Forming continuous SECTIONS
-napokat.becsuld = TRUE
-becsles.modszer = 'count' # length, vagy count, attol fuggoen, hogy minek a varianciajat minimalizaljuk
-napok           = 14 # manualisan megadott szakaszhossz, ignoraljuk, ha a becsles TRUE
-legrovidebb     = 7  # ennel rovidebb szakaszokat eldobjuk, ha ez NA, vagy negativ, akkor nincs szures
-
-f.df = sectioning( f.df, autolength = napokat.becsuld, automethod = becsles.modszer, napok = napok, minnapok = legrovidebb )
+# # Forming continuous SECTIONS
+# napokat.becsuld = TRUE
+# becsles.modszer = 'count' # length, vagy count, attol fuggoen, hogy minek a varianciajat minimalizaljuk
+# napok           = 14 # manualisan megadott szakaszhossz, ignoraljuk, ha a becsles TRUE
+# legrovidebb     = 7  # ennel rovidebb szakaszokat eldobjuk, ha ez NA, vagy negativ, akkor nincs szures
+#
+# f.df = sectioning( f.df, autolength = napokat.becsuld, automethod = becsles.modszer, napok = napok, minnapok = legrovidebb )
 
 # Translating to english (currently only used for overlap functions)
 f.df.en = english.translation( f.df )
